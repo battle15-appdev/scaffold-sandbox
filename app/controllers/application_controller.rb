e5093ffa00cd74700636c425258ec07f726d0935
battle15 <mbattle15@student.ccc.edu>
2022-11-28 17:17:56 +0000

    first and last name saves.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :first_name, :last_name, :avatar_url])
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:first_name, :last_name, :email, :password, :password_confirmation])
     
-    devise_parameter_sanitizer.permit(:account_update, :keys => [:first_name, :last_name, :avatar_url])
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:first_name, :last_name, :email, :password, :password_confirmation])
   end
+
 end
